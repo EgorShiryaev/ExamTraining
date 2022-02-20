4847ab1f1d87a9e5e36a90815a07d4ed3a5ed16f
EgorShiryaev <shiryaevvyatsu@gmail.com>
2022-02-20 23:47:41 +0300

    Изменены модели данных и добавлен класс для работы с firestore

--- lib/data/models/exam.dart
+++ lib/data/models/exam.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'exam_ticket.dart';
 
 class Exam {
@@ -7,13 +8,44 @@ class Exam {
   final Importance importance;
   final List<ExamTicket> tickets;
 
+  DocumentReference? reference;
+
   Exam({
     required this.title,
     required this.dateTime,
     required this.location,
     required this.importance,
     required this.tickets,
-   });
+    this.reference,
+  });
+
+  factory Exam.fromJson(Map<String, dynamic> json) {
+    return Exam(
+      title: json['title'],
+      dateTime: json['dateTime'],
+      location: json['location'],
+      importance: Importance.values[json['importance']],
+      tickets: (json['tickets'] as List<Map<String, dynamic>>)
+          .map((v) => ExamTicket.fromJson(v))
+          .toList(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'dateTime': dateTime,
+      'location': location,
+      'importance': importance.index,
+      'tickets': tickets.map((e) => e.toJson()).toList(),
+    };
+  }
+
+  factory Exam.fromSnapshot(DocumentSnapshot snapshot) {
+    final exam = Exam.fromJson(snapshot.data() as Map<String, dynamic>);
+    exam.reference = snapshot.reference;
+    return exam;
+  }
 }
 
 enum Importance {
